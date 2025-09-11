0b702d9b0abe28d7e7f6dae0ba7fcf80b0abd60a
user.email <manuel25142808@gmail.com>
2025-09-11 16:23:27 -0500

    Pasamos de pedir los datos completos a pedirlos por medio de ID

--- ConsumoApi/consumoCliente.php
+++ ConsumoApi/consumoCliente.php
@@ -3,13 +3,18 @@ $url = "http://localhost:8080/clientes";
 
 $consumo = file_get_contents($url);
 
-if ($consumo === false){
+if ($consumo === false) {
     die("Error al consumir el servicio");
 }
 
 $clientes = json_decode($consumo);
 
-foreach ($clientes as $cliente){
-    echo $cliente . "\n";
+foreach ($clientes as $cliente) {
+    $datos = explode(" | ", $cliente);
+    $id = (int) trim($datos[0]);
+
+    if ($id >= 3) {
+        echo $cliente . "\n";
+    }
 }
 ?>
