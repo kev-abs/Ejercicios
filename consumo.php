3358e8b5754f404f975f88368dfc8480159e7f71
SANTISALGADO <santisalgado200@outlook.com>
2025-09-11 17:11:48 -0500

    Ejercicio Santiago

---
+++ consumo.php
@@ -0,0 +1,29 @@
+<?php
+
+$url = "http://localhost:8080/resena";
+
+$consumo = file_get_contents($url);
+
+if ($consumo === false) {
+    die("Error al consumir el servicio .");
+}
+
+$resenas = json_decode($consumo);
+
+foreach ($resenas as $resena) {
+     echo $resena;
+
+    // extraer la calificación (4ª parte de la cadena)
+    $partes = explode(" | ", $resena);
+    $calificacion = (int)$partes[3];
+
+    if ($calificacion >= 5) {
+        echo " → BUENA RESEÑA\n";
+    } else {
+        echo " → MALA RESEÑA\n";
+    }
+
+
+}
+
+?>
