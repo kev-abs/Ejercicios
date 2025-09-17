e5a4b2ea629b3acb854ab5fc3de81804851162d1
Kevin-Alexis-Mendez-Vega <kevinmendez5478@gmail.com>
2025-09-17 16:34:25 -0500

    Put y Delete de la tabla ConsumoCupon

--- ConsumoApi/Ventas/ConsumoCupon.php
+++ ConsumoApi/Ventas/ConsumoCupon.php
@@ -18,6 +18,8 @@ echo "1. Mostrar todos los cupones\n";
 echo "2. Mostrar solo los cupones con 20% de descuento\n";
 echo "3. Mostrar cupones con más de 30% de descuento\n";
 echo "4. Agregar un nuevo cupon\n";
+echo "5. Actualizar un cupon\n";
+echo "6. Eliminar un cupon\n";
 
 $opcion = (int) readline("Opción: ");
 
@@ -42,9 +44,9 @@ foreach ($cupon as $c) {
 //POST
 
 if ($opcion === "4") {
-    $codigo = readline("Ingrese codigo: ");
-    $descuento = readline("Ingrese descuento: ");
-    $fecha = readline("Ingrese fecha de expiración (YYYY-MM-DD): ");
+    $codigo = readline("Ingrese el codigo: ");
+    $descuento = readline("Ingrese el descuento: ");
+    $fecha = readline("Ingrese la fecha de expiración (YYYY-MM-DD): ");
 
     $datos = array(
         "codigo" => $codigo,
@@ -76,10 +78,77 @@ if ($opcion === "4") {
     curl_close($proceso);
 
     if ($http_code === 200) {
-        echo "Usuario guardado correctamente. Respuesta (200)\n";
+        echo "Cupon guardado correctamente. Respuesta (200)\n";
     } else {
         echo "Error en el servidor. Respuesta $http_code\n";
         echo "Respuesta del servidor: $respuestapet\n";
+    }
+}
+
+//PUT
+
+if($opcion == "5") {
+
+    $id = readline("ID del cupon que desea actualizar: ");
+    $codigo = readline("Ingrese el codigo: ");
+    $descuento = readline("Ingrese el descuento: ");
+    $fecha = readline("Ingrese la fecha de expiración (YYYY-MM-DD): ");
+
+    $datos = array(
+        "codigo" => $codigo,
+        "descuento" => (float)$descuento,
+        "fecha_Expiracion" => $fecha
+
+    );
+
+    $data_json = json_encode($datos);
+
+    $proceso = curl_init("$url/$id");
+
+    curl_setopt($proceso, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($proceso, CURLOPT_POSTFIELDS, $data_json);
+    curl_setopt($proceso, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($proceso, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_json)
+    ));
+    
+
+    $respuestapet = curl_exec($proceso);
+
+    $http_code = curl_getinfo($proceso, CURLINFO_HTTP_CODE);
+
+    if (curl_errno($proceso)) {
+        die("Error en la petición PUT: " . curl_error($proceso) . "\n");
+    }
+    curl_close($proceso);
+
+    if ($http_code === 200) {
+        echo "Cupon actualizado correctamente. Respuesta (200)\n";
+    } else {
+        echo "Error en el servidor. Respuesta $http_code\n";
+        echo "Respuesta del servidor: $respuestapet\n";
+    }
+}
+
+//DELETE
+
+if ($opcion == 6) {
+    $id = readline("Ingrese ID del cupon a eliminar: ");
+
+    $proceso = curl_init("$url/$id");
+    curl_setopt($proceso, CURLOPT_CUSTOMREQUEST, "DELETE");
+    curl_setopt($proceso, CURLOPT_RETURNTRANSFER, true);
+
+    $respuestapet = curl_exec($proceso);
+    $http_code = curl_getinfo($proceso, CURLINFO_HTTP_CODE);
+    curl_close($proceso);
+
+    if ($http_code === 200 || $http_code === 204) {
+        echo "Cupon eliminado correctamente.\n";
+    } else {
+        echo "Error ($http_code): $respuestapet\n";
     }
 }
 
