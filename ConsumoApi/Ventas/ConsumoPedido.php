85c084e05acdd65c519094f138a59535300f99ae
Kevin-Alexis-Mendez-Vega <kevinmendez5478@gmail.com>
2025-09-18 17:33:55 -0500

    Consumo Pedido metodo GET no funcional, se agrega metodo POST funcional

--- ConsumoApi/Ventas/ConsumoPedido.php
+++ ConsumoApi/Ventas/ConsumoPedido.php
@@ -23,7 +23,7 @@ $opcion = (int) readline("Opción: ");
 echo "=== RESULTADO ===\n";
 
 foreach ($pedido as $p) {
-    $partes = explode(" ", $c); 
+    $partes = explode(" ", $p); 
     $id = $partes[0];
     $id_cliente = $partes[1];
     $fecha = $partes[2];
@@ -35,5 +35,50 @@ foreach ($pedido as $p) {
     }
 }
 
+//POST
+
+if ($opcion == 4) {
+    $id_cliente = readline("Ingrese el ID del Cliente: ");
+    $fecha = readline("Ingrese la fecha de pedido (YYYY-MM-DD): ");
+    $estado = readline("Ingrese el estado del pedido: ");
+    $total = readline("Ingrese el total del pedido: ");
+
+    $datos = array(
+        "id_Cliente" => $id_cliente,
+        "fecha_Pedido" => $fecha,
+        "estado" => $estado,
+        "total" => (float)$total
+    );
+
+    $data_json = json_encode($datos);
+
+    $proceso = curl_init($urlPedido);
+
+    curl_setopt($proceso, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($proceso, CURLOPT_POSTFIELDS, $data_json);
+    curl_setopt($proceso, CURLOPT_RETURNTRANSFER, true);
+
+    curl_setopt($proceso, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_json)
+    ));
+    
+
+    $respuestapet = curl_exec($proceso);
+
+    $http_code = curl_getinfo($proceso, CURLINFO_HTTP_CODE);
+
+    if (curl_errno($proceso)) {
+        die("Error en la petición POST: " . curl_error($proceso) . "\n");
+    }
+    curl_close($proceso);
+
+    if ($http_code === 200) {
+        echo "Pedido guardado correctamente. Respuesta (200)\n";
+    } else {
+        echo "Error en el servidor. Respuesta $http_code\n";
+        echo "Respuesta del servidor: $respuestapet\n";
+    }
+}
 
 ?>
