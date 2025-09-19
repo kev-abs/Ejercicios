f74843b03b6ab5e6abef05d5d95a6b03d1679be3
user.email <manuel25142808@gmail.com>
2025-09-18 21:35:42 -0500

    Confi ajustado

--- ConsumoApi/Confi.php
+++ ConsumoApi/Confi.php
@@ -4,4 +4,6 @@ $url = "http://localhost:8080/productos";
 $urlProveedores = "http://localhost:8080/proveedor";
 $urlCupon = "http://localhost:8080/cupon";
 $urlPedido = "http://localhost:8080/pedido";
+$urlCliente = "http://localhost:8080/clientes";
+$urlEmpleado = "http://localhost:8080/empleados";
 ?>
